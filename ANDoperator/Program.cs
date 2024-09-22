c38ad62beca90d4c66a7244d011529db43816770
Ly Braust <ly.braust@tktk.ee>
2024-09-22 13:54:49 +0300

    add week5 code

---
+++ ANDoperator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Math >=90; Biology >=; Chemistry >=;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemestry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted.");
+}
+else
+{ 
+    Console.WriteLine("Your applicati0n cannot be approved.");
+}
---
+++ Week5/Program.cs
@@ -0,0 +1,10 @@
+﻿//AND
+ 
+// true && true ---> true
+// kõik tingimused peavad olema tõesed, muidu on vastus vale.
+// AND  on väga range
+
+//OR
+//true|| (pipes)
+// üks tingimustest peab olema täidetud. Üks või teine (OR)
+
